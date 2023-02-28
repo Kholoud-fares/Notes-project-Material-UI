d3f3078c507a8ee34161b4aae402ce969d10e21f
Kholoud Shaker <80285150+Kholoud-fares@users.noreply.github.com>
2023-02-28 14:17:38 +0200

    Update Note.js

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -12,7 +12,7 @@ export default function Note() {
   }, [])
 
   const handleDelete = async (id) =>{
-     await fetch('http://localhost:8000/notes' + id,{
+     await fetch('http://localhost:8000/notes/' + id,{
        method: 'DELETE'
      })
 
